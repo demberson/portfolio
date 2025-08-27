613e36421ce18fd2264d8765cac2d6189dc024e9
Dillon <dillyug@gmail.com>
2025-08-27 16:38:49 -0500

    add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import Header from '../components/Header';
+import '../styles/Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <Header />
+      </div>
+  );
+}
+
+export default Home;
