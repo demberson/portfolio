dfed2886106573a12dded6cf7cc04681a351d338
Dillon <dillyug@gmail.com>
2026-01-15 17:08:29 -0600

    add egg game showcase

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,17 @@ import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import '../styles/Home.css';
 import GifToAscii from '../components/GifToAscii';
+import EggGame from '../components/EggGame/EggGame'
 
 function Home() {
   return (
     <div className="home">
       <Header />
+
       <GifToAscii />
+      
+      <EggGame />
+
       </div>
   );
 }
