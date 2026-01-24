e136e95ece2428da55eaca5338542c21c378a874
Dillon <dillyug@gmail.com>
2026-01-23 20:03:25 -0600

    include compiled wasm files

---
+++ wasm-lib/pkg/gif_to_ascii_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const convert_gif_to_ascii: (a: number, b: number, c: number) => [number, number, number, number];
+export const init_panic_hook: () => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_start: () => void;
